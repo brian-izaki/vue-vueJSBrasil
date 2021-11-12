b50a587c9c4a1b01d2cbdf9447bf6d9cb2ade87f
brian-izaki <bkizaki@outlook.com>
2021-11-12 10:53:10 -0300

    feat (auth rotas) - autenticação de usuário logado

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,11 +4,34 @@
 </template>
 
 <script>
+import { watch } from '@vue/runtime-core'
+import { useRoute, useRouter } from 'vue-router'
 import ModalFactory from './components/ModalFactory'
-export default {
+import services from './services'
 
+// manipulação de usuário logado fica aqui
+// pois, aqui é o "marco zero" onde todo componente acaba passando antes da montagem.
+
+export default {
   components: {
     ModalFactory
+  },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        const { data } = await services.users.getMe()
+        console.log('data', data)
+      }
+    })
   }
 }
 </script>
